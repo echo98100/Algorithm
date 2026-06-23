36e57807dc2d7703d9065ca23854c22ef99cd3a8
echo98100 <echo98100@gmail.com>
2026-06-23 16:38:37 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -0,0 +1,9 @@
+def solution(my_string):
+    answer = ''
+    
+    vowel = ['a', 'e', 'i', 'o', 'u']
+    
+    for i in vowel:
+        my_string = my_string.replace(i,'')
+    
+    return my_string
