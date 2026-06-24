8fbac8759b2c7b5f73d7a0681d3d59b062ae9e52
echo98100 <echo98100@gmail.com>
2026-06-24 16:48:35 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -0,0 +1,10 @@
+def solution(common):
+    answer = 0
+    
+    for i in range(3):
+        if common[1] + (common[1] - common[0]) == common[2]:
+            answer = common[len(common)-1] + (common[1] - common[0])
+        else:
+            answer = common[len(common)-1] * (common[1]/common[0])
+    
+    return answer
