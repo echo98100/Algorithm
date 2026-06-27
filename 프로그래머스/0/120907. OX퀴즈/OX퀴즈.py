2291abb100244fc277e3164cb8a8c0a290213404
echo98100 <echo98100@gmail.com>
2026-06-27 14:46:48 +0900

    [level 0] Title: OX퀴즈, Time: 0.02 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
@@ -0,0 +1,20 @@
+def solution(quiz):
+    answer = []
+    
+    for i in quiz:
+        tmp = i.split()
+        
+        if tmp[1] == '+':
+            if int(tmp[0])+int(tmp[2]) == int(tmp[4]):
+                answer.append("O")
+            else:
+                answer.append("X")
+        elif tmp[1] == '-':
+            if int(tmp[0])-int(tmp[2]) == int(tmp[4]):
+                answer.append("O")
+            else:
+                answer.append("X")
+    
+    
+    
+    return answer
