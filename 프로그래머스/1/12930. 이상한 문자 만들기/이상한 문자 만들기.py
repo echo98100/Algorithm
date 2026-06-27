84ff78194f2d175b9d1b698919c66660f3b58143
echo98100 <echo98100@gmail.com>
2026-06-27 17:30:46 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,12 @@
+def solution(s):
+    answer = ''
+    
+    for i in s.split(' '):
+        for j in range(len(i)):
+            if j==0 or j%2==0:
+                answer += i[j].upper()
+            else:
+                answer += i[j].lower()
+        answer += ' '
+        
+    return answer[:-1]
