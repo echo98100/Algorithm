5d845aab34ca43d676aa9d806e384285927c3594
echo98100 <echo98100@gmail.com>
2026-06-27 15:22:34 +0900

    [level 0] Title: 문자열 밀기, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
@@ -0,0 +1,15 @@
+def solution(A, B):
+    answer = -1
+    
+    if A == B:
+        return 0
+    
+    for i in range(len(A)):
+        
+        A = A[-1] + A[:-1]
+        
+        if(A == B):
+            return i+1
+        
+    
+    return answer
