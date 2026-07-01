182270be476977e5b200cb03254744dc64a898ff
echo98100 <echo98100@gmail.com>
2026-07-01 15:05:32 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -0,0 +1,11 @@
+def solution(s):
+    answer = True
+    
+    if len(s) != 4 and len(s) != 6:
+        return False
+    
+    for i in s:
+        if i not in '1234567890':
+            return False
+    
+    return answer
