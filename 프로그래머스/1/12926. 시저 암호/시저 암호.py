5e74a4954d5b0871f8ac7462d20d7e3866973198
echo98100 <echo98100@gmail.com>
2026-07-02 15:08:42 +0900

    [level 1] Title: 시저 암호, Time: 0.67 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -0,0 +1,13 @@
+def solution(s, n):
+    answer = ''
+    
+    for i in s:
+        if i == ' ':
+            answer += ' '
+        elif 'a' <= i <= 'z':
+            answer += chr((ord(i) - ord('a') + n) % 26 + ord('a'))
+        else:
+            answer += chr((ord(i) - ord('A') + n) % 26 + ord('A'))
+            
+    
+    return answer
