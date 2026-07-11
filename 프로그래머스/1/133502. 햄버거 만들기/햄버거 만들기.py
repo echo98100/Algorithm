821d5afd1be362a348226b89e79f0353685d30db
echo98100 <echo98100@gmail.com>
2026-07-11 14:31:15 +0900

    [level 1] Title: 햄버거 만들기, Time: 123.63 ms, Memory: 28.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,15 @@
+def solution(ingredient):
+    answer = 0
+    
+    stack = []
+    
+    for i in ingredient:
+        
+        stack.append(i)
+        
+        if len(stack) >= 4:
+            if stack[-4:] == [1,2,3,1]:
+                del stack[-4:]
+                answer += 1
+    
+    return answer
