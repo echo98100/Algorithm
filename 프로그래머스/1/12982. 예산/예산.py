c542548684a262d73988d005e568fecb74b71594
echo98100 <echo98100@gmail.com>
2026-07-15 23:10:55 +0900

    [level 1] Title: 예산, Time: 0.02 ms, Memory: 11.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -0,0 +1,10 @@
+def solution(d, budget):
+    answer = 0
+    sum = 0 
+    for i in sorted(d):
+        sum += i
+        if sum > budget:
+            break
+        answer += 1
+    
+    return answer
