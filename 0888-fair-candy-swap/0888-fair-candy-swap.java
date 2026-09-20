b7f99734bb72116c4d3b84ddee69c2cddc51a0c2
echo98100 <221263337+echo98100@users.noreply.github.com>
2026-09-20 16:25:16 +0900

    Time: 13 ms (39.85%), Space: 48.8 MB (9.48%) - LeetHub

--- 0888-fair-candy-swap/0888-fair-candy-swap.java
+++ 0888-fair-candy-swap/0888-fair-candy-swap.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Solution {
     public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
         int[] answer = new int[2];
@@ -11,6 +12,7 @@ class Solution {
             bobSum += n;
         }
 
+        Arrays.sort(bobSizes);
         // aliceSum - alice + bob = bobSum - bob + alice
         // aliceSum - bobSum = 2(alice - bob)
         // 각 alice에 대해서 만족하는 bob이 bobSizes 안에 있는지 확인 
@@ -20,14 +22,14 @@ class Solution {
 
             int temp = alice - (aliceSum - bobSum)/2;
 
-            for(int bob : bobSizes) {
-                if(temp == bob) {
-                    answer[0] = alice;
-                    answer[1] = bob;
-                    break loop;
-                }
-            }
+            int index = Arrays.binarySearch(bobSizes, temp);
 
+            if(index >= 0) {
+                answer[0] = alice;
+                answer[1] = temp;
+
+                break loop;
+            }
         }
         
         return answer;
